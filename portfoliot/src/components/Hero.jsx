1c120f1215a5a1245e4138e718948aa3bbb7e922
Himanshu Tiwari <htiwari578@gmail.com>
2024-11-17 19:21:59 +0530

    added resume

--- portfoliot/src/components/Hero.jsx
+++ portfoliot/src/components/Hero.jsx
@@ -34,7 +34,16 @@ const Hero = () => {
                     bg-clip-text text-transparent text-2xl md:text-3xl">Full-Stack Web Developer</h3>
                     <p className="md:text-sm text-pretty text-[10px] text-gray-300">
                     I am a passionate full-stack developer with expertise in React.js, Node.js, and backend technologies like MongoDB and MySQL. My proficiency extends to JavaScript and Java, allowing me to craft robust, efficient, and scalable web applications. I specialize in designing and implementing RESTful APIs, ensuring secure, high-performance communication between the frontend and backend while maintaining clean and modular architecture.
-                    </p>
+                  </p>
+                <a 
+                  href="/resume.pdf" 
+              
+                  className="text-nowrap rounded-lg border-indigo-600 bg-black px-2 py-2
+                          text-lg font-medium text-white shadow-lg shadow-indigo-700 transition-all duration-300
+                          hover:-translate-y-2 hover:shadow-xl hover:shadow-indigo-600"
+                  >
+                  Resume
+                </a>
 
             
             </motion.div>
